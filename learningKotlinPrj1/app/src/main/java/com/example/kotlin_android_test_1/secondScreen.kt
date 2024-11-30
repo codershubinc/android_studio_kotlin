d99193090a93ba0daaabd94e39c5e1cb8b0443b4
Swapnil Ingle <ingleswapnil2004@gmail.com>
2024-11-30 10:05:37 +0530

    signup screen

--- learningKotlinPrj1/app/src/main/java/com/example/kotlin_android_test_1/secondScreen.kt
+++ learningKotlinPrj1/app/src/main/java/com/example/kotlin_android_test_1/secondScreen.kt
@@ -1,32 +1,20 @@
 package com.example.kotlin_android_test_1
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
+class secondScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-    fun goToSecondScreen(view: View) {
-        val intent = Intent(this, secondScreen::class.java)
-        startActivity(intent)
-    }
-
-
 }
-
